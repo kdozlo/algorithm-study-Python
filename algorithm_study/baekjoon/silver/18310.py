2e096d65d8c4b497b0a72b5bf34de014c478f511
kdozlo <eodud5730@naver.com>
2023-02-10 18:11:30 +0900

    18310

---
+++ algorithm_study/baekjoon/silver/18310.py
@@ -0,0 +1,14 @@
+import sys
+import math
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+
+arry = list(map(int, input().split()))
+
+arry = sorted(arry)
+
+print(arry[(n - 1) // 2])
+
+
+
